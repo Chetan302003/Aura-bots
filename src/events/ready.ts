b71d9ebc7ab0505aafdabbb7bea35622b88a6b05
Chetan302003 <141828783+Chetan302003@users.noreply.github.com>
2026-02-23 15:19:12 +0530

    Log success message when bot connects to Discord

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,8 @@
 import { Client, Events } from 'discord.js';
-
+client.on("ready", () => {
+    console.log("🔥 BOT SUCCESSFULLY CONNECTED TO DISCORD");
+});
+client.login(process.env.DISCORD_TOKEN as string);
 export default {
     name: Events.ClientReady,
     once: true,
